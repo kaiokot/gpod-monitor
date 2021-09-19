d05b4d7401c5dae555b16878085a34b926cfc3ca
Kaio Turubia <kaio@stymeapp.com>
2021-09-19 01:00:02 -0300

    install pip automatic

--- install.sh
+++ install.sh
@@ -4,8 +4,23 @@ command -v pip > /dev/null
 
 if [ $? -eq 1 ]
 then
-    echo "oops, pip not installed!";
+    read -r -p "oops, pip not installed! Do you want install? [Y/n] " input
+ 
+    case $input in
+        [yY][eE][sS]|[yY])
+    echo "installing pip..."
+    apt install -qy python3-pip &
+    echo "pip installed! "
+    ;;
+        [nN][oO]|[nN])
+    echo "ok, no problem"
     exit 1
+        ;;
+        *)   
+    echo "invalid option" 
+    exit 1
+    ;;
+    esac
 fi
 
 pip install -r requirements.txt
