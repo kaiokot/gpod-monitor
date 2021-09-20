679297bf6d48584916cb30c50db0b44e1b294e1e
Kaio Turubia <kaio@stymeapp.com>
2021-09-20 12:33:52 -0300

    add python check

--- install.sh
+++ install.sh
@@ -1,6 +1,34 @@
 #!/bin/sh
 
-command -v pip > /dev/null
+
+command -v python3 > /dev/null
+
+if [ $? -eq 1 ]
+then
+    read -r -p "oops, python3 not installed! Do you want install? [Y/n] " input
+ 
+    case $input in
+        [yY][eE][sS]|[yY])
+    echo "installing python3..."
+    sudo apt install software-properties-common & 
+    sudo add-apt-repository ppa:deadsnakes/ppa &
+    sudo apt update
+    sudo apt install python3.8 &
+    echo "python3 installed! "
+    ;;
+        [nN][oO]|[nN])
+    echo "ok, no problem"
+    exit 1
+        ;;
+        *)   
+    echo "invalid option" 
+    exit 1
+    ;;
+    esac
+fi
+
+
+command -v pip3 > /dev/null
 
 if [ $? -eq 1 ]
 then
